c469243d20612e646f59ac7a9fca9d32bcd163dd
Aayush Shah <aayush.shah@daimler.com>
2019-02-28 17:41:11 -0800

    Finished Perspective Transform

--- 2-Distort_Transform/distort_and_transform.py
+++ 2-Distort_Transform/distort_and_transform.py
@@ -15,28 +15,35 @@ img = cv2.imread('test_image2.png')
 nx = 8 # the number of inside corners in x
 ny = 6 # the number of inside corners in y
 
-# MODIFY THIS FUNCTION TO GENERATE OUTPUT 
-# THAT LOOKS LIKE THE IMAGE ABOVE
 def corners_unwarp(img, nx, ny, mtx, dist):
-    # Pass in your image into this function
-    # Write code to do the following steps
-    # 1) Undistort using mtx and dist
-    # 2) Convert to grayscale
-    # 3) Find the chessboard corners
-    # 4) If corners found: 
-            # a) draw corners
-            # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
-                 #Note: you could pick any four of the detected corners 
-                 # as long as those four corners define a rectangle
-                 #One especially smart way to do this would be to use four well-chosen
-                 # corners that were automatically detected during the undistortion steps
-                 #We recommend using the automatic detection of corners in your code
-            # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
-            # d) use cv2.getPerspectiveTransform() to get M, the transform matrix
-            # e) use cv2.warpPerspective() to warp your image to a top-down view
-    #delete the next two lines
-    M = None
-    warped = np.copy(img) 
+
+    #Undistorting using mtx and dist
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+
+    #Converting to grayscale
+    gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
+
+    sizex = np.shape(gray)[1]
+    sizey = np.shape(gray)[0]
+
+    #Finding the chessboard corners for using as source points
+    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+    if ret == True:
+        # defining 4 source points
+        src = np.float32([corners[0][0], corners[nx-1][0],
+                         corners[-nx][0], corners[-1][0]])
+
+        # defining 4 destination points
+        dst = np.float32([[100,100], [sizex-100, 100],
+                         [100, sizey-100], [sizex-100, sizey-100]])
+
+        # computing the transform matrix from source and destination points
+        M = cv2.getPerspectiveTransform(src, dst)
+
+        # getting the bird's eye view
+        warped = cv2.warpPerspective(undist, M, (sizex, sizey))
+
     return warped, M
 
 top_down, perspective_M = corners_unwarp(img, nx, ny, mtx, dist)
@@ -47,3 +54,4 @@ ax1.set_title('Original Image', fontsize=50)
 ax2.imshow(top_down)
 ax2.set_title('Undistorted and Warped Image', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
